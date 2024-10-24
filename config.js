7895c4cc26797715518db68aae8e46d649888408
NethminaOFC <nethminab20@gmail.com>
2024-10-24 19:14:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=umJVFaQR#5OFcERsScmtcJyB4bmiI-lQpw-v2dPL5pDVeqsZ5qow",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WiZngSKa#nG8eSp6sW7PDPWlKh8lhLEWWXWYKe0nLN7NJnKUVz3M",
 MONGODB: process.env.MONGODB || "mongodb+srv://nethminabhashitha2009:p9nUB7v8jqlQpxsu@cluster0.dyxda.mongodb.net/",
 };
